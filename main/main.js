0ea0d71adb201e647eda922da88690505c3cdef1
SR <865459737@qq.com>
2019-05-25 15:44:02 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    const led=[["._.","|.|","|_|"],["...","..|","..|"],["._.","._|","|_."],
+                ["._.","._|","._|"],["...","|_|","..|"],["._.","|_.","._|"],
+                ["._.","|_.","|_|"],["._.","..|","..|"],["._.","|_|","|_|"],
+                ["._.","|_|","..|"]];
+
+    
+    var str=num.toString();
+    var first=str[0],ret=[led[first][0],led[first][1],led[first][2]];
+    for(let i=1;i<str.length;i++){
+        let sub=str[i];
+        ret[0]+=" "+led[sub][0];
+        ret[1]+=" "+led[sub][1];
+        ret[2]+=" "+led[sub][2];  
+    }
+   ret.push('');
+    return ret.join('\n');
 };
